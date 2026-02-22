7aa681fd27c0af704d6c4a1134fd36f8ed9bfe6d
dreaifekks <dreaifehe@gmail.com>
2026-02-22 19:24:34 +0900

    self config:remove timeline

--- src/config.ts
+++ src/config.ts
@@ -43,7 +43,7 @@ export const siteConfig: SiteConfig = {
 		friends: true, // 友链页面开关
 		projects: true, // 项目页面开关
 		skills: true, // 技能页面开关
-		timeline: true, // 时间线页面开关
+		timeline: false, // 时间线页面开关
 		albums: false, // 相册页面开关
 		devices: false, // 设备页面开关
 	},
@@ -356,11 +356,11 @@ export const navBarConfig: NavBarConfig = {
 				// 	url: "/skills/",
 				// 	icon: "material-symbols:psychology",
 				// },
-				{
-					name: "Timeline",
-					url: "/timeline/",
-					icon: "material-symbols:timeline",
-				},
+				// {
+				// 	name: "Timeline",
+				// 	url: "/timeline/",
+				// 	icon: "material-symbols:timeline",
+				// },
 			],
 		},
 	],
